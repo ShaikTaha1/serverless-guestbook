2e0a8bb94786e8b17137fe93d4535f445ff660ef
ShaikTaha1 <shaiktaha14@gmail.com>
2021-08-21 07:28:31 +0000

    Temporary changes

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5ee93856.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
